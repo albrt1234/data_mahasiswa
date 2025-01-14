5b682961ba671e2aab9fc147cf1c9db9e2f5f564
albrt1234 <lolalbert777@gmail.com>
2025-01-15 00:39:05 +0700

    crud.sql

--- db/crud.sql
+++ db/crud.sql
@@ -27,22 +27,20 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `peserta` (
-  `id_peserta` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `nama` varchar(50) NOT NULL,
-  `sekolah` varchar(50) NOT NULL,
-  `jurusan` varchar(50) NOT NULL,
-  `no_hp` char(13) NOT NULL,
-  `alamat` varchar(50) NOT NULL
+  `npm` varchar(50) NOT NULL,
+  `kelas` varchar(50) NOT NULL,
+  `jurusan`varchar(13) NOT NULL,
+  `fakultas` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `peserta`
 --
 
-INSERT INTO `peserta` (`id_peserta`, `nama`, `sekolah`, `jurusan`, `no_hp`, `alamat`) VALUES
-(1, 'RADHEN ADEBOS', 'SMK BANDUNG 21', 'TKJ', '08212121212', 'KIARA CONDONG TENGAH'),
-(3, 'RADHEN ADEBOS', 'SMK PUTRA PAJAJARAN', 'MULTIMEDIA', '081212312', 'BANDUNG');
-
+INSERT INTO `peserta` (`id`, `nama`, `npm`, `kelas`, `jurusan`, `fakultas`) VALUES
+(1, 'RADHEN ADEBOS', 'bob', '5042345', 'informatika', 'teknologi industri');
 --
 -- Indexes for dumped tables
 --
@@ -51,7 +49,7 @@ INSERT INTO `peserta` (`id_peserta`, `nama`, `sekolah`, `jurusan`, `no_hp`, `ala
 -- Indexes for table `peserta`
 --
 ALTER TABLE `peserta`
-  ADD PRIMARY KEY (`id_peserta`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -61,7 +59,7 @@ ALTER TABLE `peserta`
 -- AUTO_INCREMENT for table `peserta`
 --
 ALTER TABLE `peserta`
-  MODIFY `id_peserta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
